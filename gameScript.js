58115cb6d561bedf828a2fd87d28917887cd66e7
n0ll0 <101214789+n0ll0@users.noreply.github.com>
2024-08-21 21:58:09 +0300

    updated to using a class

--- gameScript.js
+++ gameScript.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
+const layout = {
+  yaxis: {range: [1, 9], title: "score"},
+}
+
+let scores = [];
 
 let attempts = 0;
 let gameNumber = 0;
 let currentScore = 0;
+const message = document.getElementById("message");
+const playAgain = document.getElementById("playAgain");
 
 // Get previous game data from local storage
 if (localStorage.getItem("gameNumber") && localStorage.getItem("currentScore")) {
@@ -11,70 +18,168 @@ if (localStorage.getItem("gameNumber") && localStorage.getItem("currentScore")) 
 
   scoreElement.textContent = `${currentScore} / ${gameNumber} = ${currentScore / gameNumber} average`;
 }
+if (localStorage.getItem("scores")) {
+  scores = JSON.parse(localStorage.getItem("scores"));
+  Plotly.newPlot("stats", [
+    {
+      y: scores,
+      type: "lines",
+    },
+  ],layout);
+}
 
-const buttons = document.querySelectorAll(".button");
-let treasureButton;
+
+
+const buttons = document.querySelectorAll("td.button");
 
 /**
  * Select the treasure button at random
+ * @returns {Element}
  */
-function randomizeTreasure() {
-  const treasureRow = Math.floor(Math.random() * 3);
-  const treasureCol = Math.floor(Math.random() * 3);
-  treasureButton = buttons[treasureRow * 3 + treasureCol];
+function randomizeTreasure(btns) {
+  return btns[Math.floor(Math.random()*btns.length)];
 }
 
-/**
- * Handle the button click
- * @param {EventListenerOrEventListenerObject} event
- */
-function buttonClick(event) {
-  const button = event.target;
-  
-  if (button === treasureButton) {
-    if (button.style.backgroundColor === "green") return;
+class Game {
+  #treasure;
+  #buttons;
+  ended;
+  constructor(btns){
+    this.attempts = 0;
+    this.ended = false;
+    this.#buttons = btns;
+    this.#randomizeTreasure();
+    this.#buttons.forEach(btn => {btn.onclick = this.#clickButton.bind(this);});
+  }
 
-    attempts++;
-    gameNumber++;
+  #randomizeTreasure() {
+    this.#treasure = this.#buttons[Math.floor(Math.random()*this.#buttons.length)];
+  }
 
-    message.textContent = "You found the treasure!";
-    event.target.style.backgroundColor = "green";
-    playAgain.style.display = "block";
-
-    let calculatedScore = currentScore + 10 - attempts;
-    scoreElement.textContent = `(+${10-attempts}) ${calculatedScore} / ${gameNumber} = ${calculatedScore / gameNumber} average`;
-
-    currentScore = calculatedScore;
+  #clickButton(event) {
+    const button = event.target;
     
-    buttons.forEach((BUTTON) => {
-      if (BUTTON !== treasureButton) {
-        BUTTON.style.backgroundColor = "red";
-      }
+    if (button.getAttribute("clicked")) return;
+    
+    this.attempts += 1;
+    
+    button.setAttribute("clicked",true);
+    
+    if (button === this.#treasure) return this.#endGame();
+
+  }
+
+  #endGame() {
+    this.ended = true;
+    this.#treasure.setAttribute("treasure", true);
+    this.#buttons.forEach(btn=>{
+      btn.onclick = null;
     });
 
+    let calculatedScore = currentScore + 10 - this.attempts;
+    gameNumber++;
+    scoreElement.textContent = `(+${10 - this.attempts}) ${calculatedScore} / ${gameNumber} = ${
+      calculatedScore / gameNumber
+    } average`;
+
+    currentScore = calculatedScore;
     // save to local storage
     localStorage.setItem("gameNumber", JSON.stringify(gameNumber));
     localStorage.setItem("currentScore", JSON.stringify(currentScore));
-
-  } else if (button.style.backgroundColor !== "red") {
-    attempts++;
-
-    message.textContent = "Try again!";
-    event.target.style.backgroundColor = "red";
+    localStorage.setItem("scores", JSON.stringify(scores));
   }
+
+  killGame() {
+    this.#treasure.removeAttribute("treasure");
+    this.#buttons.forEach(btn=>{
+      btn.removeAttribute("treasure")
+      btn.removeAttribute("clicked")
+    });
+  }
+
 }
 
+
+// /**
+//  * Handle the button click
+//  * @param {EventListener | EventListenerObject} event
+//  */
+// function buttonClick(event) {
+//   const button = event.target;
+
+//   if (button === treasureButton) {
+//     if (button.style.backgroundColor === "green") return;
+
+//     attempts++;
+//     // gameNumber++;
+
+//     message.textContent = "You found the treasure!";
+//     event.target.style.backgroundColor = "green";
+//     playAgain.style.display = "block";
+
+//     scores.push(10 - attempts);
+
+//     let calculatedScore = currentScore + 10 - attempts;
+//     scoreElement.textContent = `(+${10 - attempts}) ${calculatedScore} / ${gameNumber} = ${
+//       calculatedScore / gameNumber
+//     } average`;
+
+//     currentScore = calculatedScore;
+
+//     buttons.forEach((BUTTON) => {
+//       if (BUTTON !== treasureButton) {
+//         BUTTON.style.backgroundColor = "red";
+//       }
+//     });
+
+//     // save to local storage
+//     localStorage.setItem("gameNumber", JSON.stringify(gameNumber));
+//     localStorage.setItem("currentScore", JSON.stringify(currentScore));
+//     localStorage.setItem("scores", JSON.stringify(scores));
+//   } else if (button.style.backgroundColor !== "red") {
+//     attempts++;
+
+//     message.textContent = "Try again!";
+//     event.target.style.backgroundColor = "red";
+//   }
+// }
+
+// /**
+//  * Handle the play again button click
+//  */
+// function playAgainClick() {
+//   buttons.forEach((e) => (e.style.backgroundColor = "#eee"));
+//   attempts = 0;
+
+//   randomizeTreasure();
+
+//   message.textContent = "";
+//   playAgain.style.display = "none";
+//   Plotly.newPlot("stats", [
+//     {
+//       y: scores,
+//       type: "lines",
+//     },
+//   ],layout);
+// }
+var game;
 /**
  * Handle the play again button click
  */
-function playAgainClick() {
-  buttons.forEach((e) => (e.style.backgroundColor = "#eee"));
-  attempts = 0;
+function startNewGame() {
+  if (game) {
+    game.killGame()
+  };
 
-  randomizeTreasure();
+  game = new Game(buttons);
 
   message.textContent = "";
-  playAgain.style.display = "none";
+  Plotly.newPlot("stats", [
+    {
+      y: scores,
+      type: "lines",
+    },
+  ],layout);
 }
 
 function resetGameStats() {
@@ -82,6 +187,12 @@ function resetGameStats() {
   window.location.reload();
 }
 
-buttons.forEach((button) => button.addEventListener("click", buttonClick));
+// buttons.forEach((button) => button.addEventListener("click", buttonClick));
+message.textContent = "";
+playAgain.style.display = "none";
 
-randomizeTreasure();
+// randomizeTreasure();
+
+// new Game(buttons);
+
+startNewGame();
